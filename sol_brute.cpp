cd2e35353f8a306ac19ed63b1286c7f7f897ef03
Krzysztof Maziarz <krzysztof.s.maziarz@gmail.com>
2017-02-23 20:05:57 +0100

    Brute added

---
+++ sol_brute.cpp
@@ -0,0 +1,40 @@
+#include "sol_brute.h"
+
+double scoreForVideoInCache(int videoId, int cacheId, Output &output) {
+    int initScore = output.getScore();
+    output.addVideoToCache(videoId, cacheId);
+    
+    int newScore = output.getScore();
+    output.removeVideoFromCache(videoId, cacheId);
+    
+    int gain = newScore - initScore;
+    return (double) gain / videoSizes[videoId];
+}
+
+bool insertBestVideoCachePair(Output &output) { // videoId, cacheId
+    int bestVideo = -1, bestCache = -1;
+    double bestScore = 1e-9;
+    
+    for (int videoId = 0; videoId < nVideos; videoId++) {
+        for (int cacheId = 0; cacheId < nCaches; cacheId++) {
+            double score = scoreForVideoInCache(videoId, cacheId, output);
+            
+            if (score > bestScore) {
+                bestScore = score;
+                bestVideo = videoId;
+                bestCache = cacheId;
+            }
+        }
+    }
+    
+    if (bestVideo != -1) {
+        output.addVideoToCache(bestVideo, bestCache);
+        return true;
+    }
+    
+    return false;
+}
+
+void solveBrute(Output &output) {
+    while (insertBestVideoCachePair(output)) {}
+}
