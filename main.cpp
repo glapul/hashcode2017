736cb9457a7b78b342b5a2bdc26258f62c9f878c
Michał Glapa <michal.glapa@gmail.com>
2017-02-23 19:57:25 +0100

    output+testy

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@
 int32_t main() {
 	readInput();
 	Output o;
+    for (int i = 0; i < nCaches; i++) {
+        o.addVideoToCache(0, i);
+    }
 	o.printOutput();
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
- for i in data/*.in; do echo $i; ./binary < $i > ${i/\.in/\.out}; done
+#!/bin/bash
+for i in data/*.in; do echo $i; ./binary < $i > ${i/\.in/\.out}; done
